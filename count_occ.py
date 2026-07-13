af04a142053177cf30df312d2855dbaf1d688e74
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-07-13 19:20:19 +0530

    Create count_occ.py

---
+++ count_occ.py
@@ -0,0 +1,35 @@
+n=int(input())
+arr=list(map(int,input().split()))
+target=int(input())
+def solution():
+    
+    i=0
+    j=n-1
+    first=-1
+    last=-1
+
+    while i<=j:
+        mid=(i+j)//2
+        if arr[mid]==target:
+            first=mid
+            j=mid-1
+        elif arr[mid]>target:
+            j=mid-1
+        else:
+            i=mid+1
+   
+    i=0
+    j=n-1
+    while i<=j:
+        mid=(i+j)//2
+        if arr[mid]==target:
+            last=mid
+            i=mid+1
+        elif arr[mid]>target:
+            j=mid-1
+        else:
+            i=mid+1
+    count=last-first+1
+    return count
+ans=solution()
+print(ans)
