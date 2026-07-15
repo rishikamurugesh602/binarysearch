70b3b33df55b18d9f91b413cc64c761081344616
rishikamurugesh602 <rishikamurugesh@gmail.com>
2026-07-15 19:21:34 +0530

    Create peak.py

---
+++ peak.py
@@ -0,0 +1,13 @@
+def solution(nums):
+    i = 0
+    j = len(nums) - 1
+
+    while i < j:
+        mid = (i + j) // 2
+
+        if nums[mid] < nums[mid + 1]:
+            i = mid + 1
+        else:
+            j = mid
+
+    return i
